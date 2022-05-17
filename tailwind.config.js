e8c47ab913920db21db49e07e45d4f5101513458
unknown <Rahmat0177>
2022-05-17 00:16:27 -0700

    header section add

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ module.exports = {
     "./public/**/*.html"
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      colors:{
+        'old-rose-color' : '#c28b7c',
+        'Mud-color'  : '#735241',
+      },
+      fontFamily:{
+        SpaceMonoRegular: ["SpaceMono-Regular, sans-serif"],
+        SpaceMonoItalic: ["SpaceMono-Italic, sans-serif"],
+        SpaceMonoBold: ["SpaceMono-Bold, sans-serif"],
+        SpaceMonoBoldItalic: ["SpaceMono-BoldItalic, sans-serif"],
+
+        BioRhymeLight: ["BioRhyme-Light, sans-serif"],
+        BioRhymeExtraLight: ["BioRhyme-ExtraLight, sans-serif"],
+        BioRhymeRegular: ["BioRhyme-Regular, sans-serif"],
+        BioRhymeBold: ["BioRhyme-Bold, sans-serif"],
+        BioRhymeExtraBold: ["BioRhyme-ExtraBold, sans-serif"]
+
+      },
+    },
   },
   plugins: [],
 }
